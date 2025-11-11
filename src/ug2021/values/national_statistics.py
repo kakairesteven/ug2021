beb1318060641d1d07a732d4cbb858379011d4ac
Hassan Kibirige <has2k1@gmail.com>
2025-11-11 19:12:18 +0300

    Use valid_votes instead of valid_voters

--- src/ug2021/values/national_statistics.py
+++ src/ug2021/values/national_statistics.py
@@ -2,13 +2,13 @@ from ..data.results import national_wide
 
 __all__ = (
     "registered_voters",
-    "valid_voters",
+    "valid_votes",
     "total_votes",
     "turnout",
 )
 
 
 registered_voters: float = national_wide["registered"][0]
-valid_voters: float = national_wide["valid"][0]
+valid_votes: float = national_wide["valid"][0]
 total_votes: float = national_wide["total"][0]
 turnout: float = national_wide["turnout"][0]
